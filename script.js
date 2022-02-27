3fb7fcc7cf256a92287bc734b7874df4712ae294
Milos <94387892+MilosM00@users.noreply.github.com>
2022-02-27 20:43:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -85,3 +85,15 @@ btnHold.addEventListener(`click`, function(){
 });
 
 btnNew.addEventListener(`click`, init);
+
+//  Pig Game
+
+//  About the game:
+
+// This game is very fun and simple. Also, what is so good about this game is that you can play it with your friend.
+// Two players race to reach 100 points.
+// A player repeatedly rolls dice until either a 1 is rolled or the player holds.
+// If a player rolls 1 every roll before that is meaningless.
+// But if the player holds, every sum roll before that is inserted in results.
+
+// There is maybe more to explain, but you will figure it out within the game.
